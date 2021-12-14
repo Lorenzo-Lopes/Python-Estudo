7555d1a4bdb76e5cd18b478e606727a96cc3362a
Lorenzo Lopes <lorenzolopes.dev@gmail.com>
2021-12-14 14:15:24 -0300

    aula 4 do curso de python3 -orientação a objetos
    Métodos de leitura dos atributos, os getters
    Métodos de modifição dos atributos, os setters
    Propriedades

--- Alura_python3_curso_pt3/conta.py
+++ Alura_python3_curso_pt3/conta.py
@@ -18,3 +18,18 @@ class Conta:
         self.saca(valor)
         destino.deposita(valor)
 
+    def get_numero(self):
+        return self.__numero
+
+    def get_titular(self):
+        return self.__titular
+    
+    def get_saldo(self):
+        return self.__saldo
+
+    @property
+    def limite(self):
+        return self.__limite
+    @limite.setter
+    def limite(self, limite):
+        self.__limite = limite
