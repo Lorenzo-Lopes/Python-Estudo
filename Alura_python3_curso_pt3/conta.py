948f89d936f3edb36ef134980c14d13c10184499
Lorenzo Lopes <lorenzolopes.dev@gmail.com>
2021-12-14 10:00:26 -0300

    aulas 1 e 2 do curso de python3 -orientação a objetos
    Classes
    Objetos
    Função construtora
    Endereço e referência de objetos
    Atributos de classe
    Acesso aos atributos através do objeto

---
+++ Alura_python3_curso_pt3/conta.py
@@ -0,0 +1,7 @@
+class Conta:
+    def __init__(self, numero, titular, saldo, limite):
+        print('contruindo objeto ....{}'.format(self))
+        self.numero=123
+        self.titulo = "lolo"
+        self.saldo =55
+        self.limite = 1000
